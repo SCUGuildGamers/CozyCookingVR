e840a198608d8649a6445b7ab9aae38a26b46935
SCU Imaginarium (Hornet) <dinouye@scu.edu>
2024-04-14 22:04:01 -0700

    Implemented Faucet Sounds
    - also fixed audio lerping to use IEnumerators
    - remade tomato sound

---
+++ Assets/Audio/Loopable.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using UnityEngine;
+
+public class Loopable : MonoBehaviour
+{
+    public IEnumerator Lerp(float volume, float delay)
+    {
+        AudioSource loopable = GetComponent<AudioSource>();
+
+        float currentTime = 0f;
+        float prevVolume = loopable.volume;
+
+        while (currentTime < delay)
+        {
+            loopable.volume = Mathf.Lerp(prevVolume, volume, currentTime / delay);
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+        loopable.volume = volume;
+    }
+
+    public IEnumerator DestroySelf(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(this.gameObject);
+    }
+
+    public IEnumerator LerpDestroySelf(float volume, float delay)
+    {
+        AudioSource loopable = GetComponent<AudioSource>();
+
+        float currentTime = 0f;
+        float prevVolume = loopable.volume;
+
+        while (currentTime < delay)
+        {
+            loopable.volume = Mathf.Lerp(prevVolume, volume, currentTime / delay);
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+        loopable.volume = volume;
+
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Faucet/Faucet.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Faucet : MonoBehaviour
+{
+    [SerializeField]
+    bool isOn = false;
+    GameObject currentLoop = null;
+
+    public void Activate()
+    {
+        AudioManager.instance.Play(Random.value > 0.5 ? "faucetsqueak_1_oneshot" : "faucetsqueak_2_oneshot", transform);
+        if (!isOn)
+        {
+            //turn on
+            currentLoop = AudioManager.instance.LerpLoopable("faucetrunning_loop", transform, 1.0f, 2.0f);
+            isOn = true;
+        }
+        else
+        {
+            //turn off
+            if (currentLoop != null)
+            {
+                StartCoroutine(currentLoop.GetComponent<Loopable>().LerpDestroySelf(0.0f, 2.0f));
+                currentLoop = null;
+            }
+            isOn = false;
+        }
+    }
+}
