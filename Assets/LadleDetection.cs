b471e9c909ac64ee5e845895957f24e4eb03c8de
Daniel Lin <dlin@scu.edu>
2024-05-06 18:53:54 -0700

    Soup Pouring mechanic
    tags and scripts to "fill" and empty a ladle with soup, doesn't fill up the bowl yet

---
+++ Assets/LadleDetection.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadleDetection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject SoupObject;
+
+    public bool hasSoup = false;
+    void Start()
+    {
+        SoupObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+        
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Ladle")
+        {
+            SoupObject.SetActive(true);
+            hasSoup = true;
+            Debug.Log("soup in ladle");
+        }   
+        if(other.gameObject.tag == "Bowl")
+        {
+            SoupObject.SetActive(false);
+            hasSoup = false;
+            // call some function with getcomponent to increase the size/height of the soup level in the bowl
+            Debug.Log("soup in bowl");
+        }
+       
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+
+}
