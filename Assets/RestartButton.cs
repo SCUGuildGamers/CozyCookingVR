a86bb8c93be7e31a2ec7a744f0dc293ac09e47fe
SCU Imaginarium (Hornet) <dinouye@scu.edu>
2024-05-16 13:01:12 -0700

    Added functionality to Play Again button
    - removed sample scene from build settings

---
+++ Assets/RestartButton.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartButton : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        StartCoroutine(SwitchScenes());
+    }
+
+    private IEnumerator SwitchScenes()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(sceneName: "Home Kitchen Original");
+    }
+}
